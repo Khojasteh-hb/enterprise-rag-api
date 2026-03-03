896a3dde6398b8a324fef913f94178221b4a712b
Hakimeh Khojasteh <khojasteh.hb@gmail.com>
2026-03-03 11:11:10 +0100

    Fix text splitter import for latest Langchain version

--- rag_index.py
+++ rag_index.py
@@ -3,7 +3,7 @@ import time
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
