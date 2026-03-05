7ba1fba34ffd855a1002b51d0589b2b0dfe8c8b6
Hakimeh Khojasteh <khojasteh.hb@gmail.com>
2026-03-05 17:00:23 +0100

    refactor: move ask.py and build_index.py into app module

---
+++ app/build_index.py
@@ -0,0 +1,46 @@
+import os
+import time
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+def create_index(file_path: str):
+    load_dotenv()
+
+    print("Loading document...")
+    loader = TextLoader(file_path)
+    documents = loader.load()
+
+    print("Splitting into chunks...")
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=150
+    )
+    chunks = splitter.split_documents(documents)
+
+    print(f"Created {len(chunks)} chunks")
+
+    print("Creating embeddings...")
+    start_time = time.time()
+
+    embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+
+    end_time = time.time()
+    print(f"Embedding time: {round(end_time - start_time, 2)} seconds")
+
+    print("Saving FAISS index locally...")
+    vectorstore.save_local("faiss_index")
+
+    print("Index created successfully!")
+
+
+if __name__ == "__main__":
+    create_index("data/sample.txt")
