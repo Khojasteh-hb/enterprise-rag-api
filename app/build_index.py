8280bf64fd7fd66606df6fa4f148ba3ec6234200
Hakimeh Khojasteh <khojasteh.hb@gmail.com>
2026-03-05 18:20:33 +0100

    refactor: improve build_index path handling and vectorstore storage

--- app/build_index.py
+++ app/build_index.py
@@ -9,7 +9,14 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 
+# ---- Paths ----
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "data", "sample.txt")
+INDEX_PATH = os.path.join(BASE_DIR, "vectorstore", "faiss_index")
+
+
 def create_index(file_path: str):
+
     load_dotenv()
 
     print("Loading document...")
@@ -21,6 +28,7 @@ def create_index(file_path: str):
         chunk_size=800,
         chunk_overlap=150
     )
+
     chunks = splitter.split_documents(documents)
 
     print(f"Created {len(chunks)} chunks")
@@ -29,18 +37,19 @@ def create_index(file_path: str):
     start_time = time.time()
 
     embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
     vectorstore = FAISS.from_documents(chunks, embeddings)
 
     end_time = time.time()
     print(f"Embedding time: {round(end_time - start_time, 2)} seconds")
 
     print("Saving FAISS index locally...")
-    vectorstore.save_local("faiss_index")
+    vectorstore.save_local(INDEX_PATH)
 
     print("Index created successfully!")
 
 
 if __name__ == "__main__":
-    create_index("data/sample.txt")
+    create_index(DATA_PATH)
