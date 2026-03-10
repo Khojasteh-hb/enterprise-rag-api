726e45dd27f8442f8184417798485225d7607102
Hakimeh Khojasteh <khojasteh.hb@gmail.com>
2026-03-10 13:33:50 +0100

    Change the path of server.py

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Building FAISS index..."
 python app/build_index.py
 
 echo "Starting FastAPI server..."
-uvicorn server:app --host 0.0.0.0 --port 8000
+uvicorn app.server:app --host 0.0.0.0 --port 8000
